293609009206ecac708678205de921c373bc530b
dkokonkwo <d.okonkwo@alustudent.com>
2023-07-13 14:56:28 +0200

    test this

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+Queries REDDIT API and returns number of 
+subscribers for a given subreddit
+'''
+
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    '''
+    returns number of subcribers if subreddit
+    is valid
+    '''
+
+    subredditURL = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    subreddit_info = requests.get(
+        subredditURL,
+        headers={'user-agent': 'user'},
+        allow_redirects=False
+    ).json()
+
+    if "data" not in subreddit_info:
+        return 0
+    subscriber_count = subreddit_info.get('data').get('subscribers')
+    return subscriber_count
